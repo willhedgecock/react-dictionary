4342327a124f0f5f6e0cf68c46fe5869b2ae4ee8
Riley Hawbaker <riley@webspec.com>
2023-10-16 09:34:54 -0500

    change responsibilite of searching to main and used output to show results

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,28 +1,44 @@
 import { useState, useEffect } from "react";
 import Input from "./Input";
+import Output from "./Output";
 import Header from "./Header";
 
 const Main = () => {
+  const [fontFamily, setFontFamily] = useState("Arial");
+  const [userInput, setUserInput] = useState("");
+  const [results, setResults] = useState([]);
 
-    const [fontFamily, setFontFamily] = useState('Arial');
-    const [userInput, setUserInput] = useState('');
+  //Update state based on Header child
+  const handleFontChange = (newFont) => {
+    setFontFamily(newFont);
+  };
 
-    //Update state based on Header child
-    const handleFontChange = (newFont) => {
-        setFontFamily(newFont)
+  //Update state based on Input child
+  const handleInput = (newWord) => {
+    setUserInput(newWord);
+  };
+
+  const handleSearch = async () => {
+    try {
+      const data = await (
+        await fetch(
+          `https://api.dictionaryapi.dev/api/v2/entries/en/${userInput}`
+        )
+      ).json();
+      setResults(data);
+    } catch (err) {
+      console.log(err.message);
+      //   setResults(data);
     }
+  };
 
-    //Update state based on Input child
-    const handleInput = (newWord) => {
-        setUserInput(newWord);
-    }
-
-    return (
-        <main style={{fontFamily}}>
-            <Header change = {handleFontChange} />
-            <Input change = {handleInput} />
-        </main>
-    )
-}
+  return (
+    <main style={{ fontFamily }}>
+      <Header change={handleFontChange} />
+      <Input change={handleInput} search={handleSearch} />
+      <Output results={results} />
+    </main>
+  );
+};
 
 export default Main;
